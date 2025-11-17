a63d4e9fee29f470f8ea1fce38816241533aae10
vdbotto <125357262+vdbotto@users.noreply.github.com>
2025-11-17 21:15:20 +0100

    Add files via upload

---
+++ launch_site.jl
@@ -0,0 +1,55 @@
+import Pkg
+Pkg.add("Genie")
+Pkg.add("FileIO")
+Pkg.add("Images")
+Pkg.add("ImageMagick")
+Pkg.add("Base64")
+Pkg.add("Dates")
+Pkg.add("XLSX")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("SMTPClient")
+Pkg.add("Random")
+Pkg.add("SHA")
+Pkg.add("Printf")
+Pkg.add("JSON")
+Pkg.add("QRCoders")
+
+using Genie
+using Genie.Router
+using Genie.Renderer.Html
+using FileWatching
+using XLSX
+using Dates
+using CSV
+using DataFrames
+using Genie.Requests
+using SMTPClient
+using Random
+using SHA
+using Printf
+using JSON
+using FileIO
+using Images
+using ImageMagick
+using Base64
+using QRCoders
+
+filepath = "BalPopo/Bal popo website actual.jl"
+
+include(filepath)
+up(host="10.67.110.167")
+
+
+# automatic up() when file is changed
+@async while true
+    url ="https://raw.githubusercontent.com/vdbotto/BalPopo177/refs/heads/main/BalPopo/Bal%20popo%20website%20actual.jl"
+    Downloads.download(url,"BalPopo/Bal popo website actual.jl")
+    sleep(10)
+    new = read(filepath,String)
+    if prev !== new
+        print("Site updated @time $(now())")
+        include(filepath)
+        up(host="10.67.110.167")
+    end
+end
