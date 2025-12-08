887613b00fb7acba147a4742783c09ed41d13cb7
vdbotto <125357262+vdbotto@users.noreply.github.com>
2025-12-08 20:01:38 +0100

    Update event schedule description for dance party

--- BalPopo/Bal popo website actual.jl
+++ BalPopo/Bal popo website actual.jl
@@ -1443,7 +1443,7 @@ route("/theevent") do
         <li><span class="time">19:00</span><span class="sep">  —  </span><span class="desc">Reception</span></li>
         <li><span class="time">20:00</span><span class="sep">  —  </span><span class="desc">Start dinner</span></li>
         <li><span class="time">21:00</span><span class="sep">  —  </span><span class="desc">Dessert and Tombola</span></li>
-        <li><span class="time">22:00</span><span class="sep">  —  </span><span class="desc">The roof flies off!</span></li>
+        <li><span class="time">22:00</span><span class="sep">  —  </span><span class="desc">Start dance party: the roof flies off!</span></li>
         <li><span class="time">03:00</span><span class="sep">  —  </span><span class="desc">The roof gets put back on again</span></li>
     </ul>
     </br>
